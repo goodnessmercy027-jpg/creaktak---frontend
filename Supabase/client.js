7547863d7cf03c1ec195f6b44c8b904a6f3ad1db
goodnessmercy027-jpg <goodnessmercy027@gmail.com>
2026-02-20 10:36:37 +0100

    Create client.js

---
+++ Supabase/client.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://djtcpclupmrfqaiyzlld.supabase.co',
+  'sb_publishable_iKe_5h9f645I-9mTJb8RFA_fAa2CSOq'
+)
