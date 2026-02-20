75f498857bef388785d687a314e068f7ec141ea0
goodnessmercy027-jpg <goodnessmercy027@gmail.com>
2026-02-20 13:52:17 +0100

    Update and rename pages/dashboard.js to pages/dashboard/client.js

---
+++ pages/dashboard/client.js
@@ -0,0 +1,11 @@
+export default function ClientDashboard() {
+  return (
+    <div className="grid">
+      <div className="card">Browse Creators</div>
+      <div className="card">My Projects</div>
+      <div className="card">Progress Tracker</div>
+      <div className="card">Team Chat</div>
+      <div className="card">Files</div>
+    </div>
+  )
+}
