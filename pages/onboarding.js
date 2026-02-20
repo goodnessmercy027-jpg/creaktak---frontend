e5a7a7e7ce759d0aad9059b57e71189fdeda3e4b
goodnessmercy027-jpg <goodnessmercy027@gmail.com>
2026-02-20 13:58:24 +0100

    Create onboarding.js

---
+++ pages/onboarding.js
@@ -0,0 +1,10 @@
+export default function Onboarding() {
+  return (
+    <div className="grid">
+      <div className="card">Create, Execute & Succeed</div>
+      <div className="card">AI Research & Design</div>
+      <div className="card">Track & Get Paid</div>
+      <a href="/login"><button>Continue</button></a>
+    </div>
+  )
+    }
