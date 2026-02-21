d6c4e108a0bfddba2bc3f633f3000e365c39fad4
goodnessmercy027-jpg <goodnessmercy027@gmail.com>
2026-02-21 13:20:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['djtcpclupmrfqaiyzlld.supabase.co']
-  }
+module.exports = {
+  reactStrictMode: true
 }
-
-module.exports = nextConfig
