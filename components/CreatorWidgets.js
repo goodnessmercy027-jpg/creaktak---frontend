85aa0480b5b191720a175e9f7041f8a55ea707d8
goodnessmercy027-jpg <goodnessmercy027@gmail.com>
2026-02-21 12:39:21 +0100

    Update and rename CreatorsWidget.js to CreatorWidgets.js

---
+++ components/CreatorWidgets.js
@@ -0,0 +1,8 @@
+export default function CreatorWidgets({ role }) {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Welcome, {role} Creator</h2>
+      <p>Your dashboard widgets will appear here.</p>
+    </div>
+  );
+}
