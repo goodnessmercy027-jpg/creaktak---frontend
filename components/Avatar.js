7e738675fe50ca42564c1c505735e9c02a96e86c
goodnessmercy027-jpg <goodnessmercy027@gmail.com>
2026-02-21 13:18:22 +0100

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,11 +1,3 @@
-import React from 'react'
-
-export default function Avatar({ src, size = 40 }) {
-  return (
-    <img
-      src={src || '/default-avatar.png'}
-      className="rounded-full"
-      style={{ width: size, height: size }}
-    />
-  )
+export default function Avatar() {
+  return <div>👤 Creator Avatar</div>
 }
