8f46308f5c793acede39f0b98a92b3f1da150c9f
goodnessmercy027-jpg <goodnessmercy027@gmail.com>
2026-02-21 13:17:52 +0100

    Update ProgressTracker.js

--- components/ProgressTracker.js
+++ components/ProgressTracker.js
@@ -1,9 +1,3 @@
-import React from 'react'
-
-export default function ProgressTracker({ progress = 0 }) {
-  return (
-    <div className="bg-gray-200 rounded-full h-4 w-full mt-2">
-      <div className="bg-[#6C2BD9] h-4 rounded-full" style={{ width: `${progress}%` }}></div>
-    </div>
-  )
-    }
+export default function ProgressTracker() {
+  return <div>Project Progress: 0%</div>
+}
